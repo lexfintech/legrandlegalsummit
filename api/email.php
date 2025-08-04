d60de08f11694322fa7a68bfcaefa79395bc54d2
satishccy-lex <satishccy-lex@users.noreply.github.com>
2025-08-04 17:12:24 +0000

    deploy: a18427e5f5c29aeb0af6dfe93a77ceba3c3d9e18

--- api/email.php
+++ api/email.php
@@ -1,8 +1,14 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Max-Age: 86400');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
